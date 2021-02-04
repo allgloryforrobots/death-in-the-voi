4d10ae2e60037c6aa79a52ae5b8151d18980114a
allgloryforrobots <75021483+allgloryforrobots@users.noreply.github.com>
2021-02-04 21:45:44 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import "fullpage.js/vendors/scrolloverflow" // Optional. When using scrollOverflow:true
+import ReactFullpage from "@fullpage/react-fullpage"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./styles.css"
+import './bg.css'
+class App extends React.Component {
+
+    state = {
+        current: 0
+    }
+
+    onLeave(origin, destination, direction) {
+        console.log("Leaving section " + origin.index)
+        this.setState({
+            current: origin.index
+        })
+    }
+    afterLoad(origin, destination, direction) {
+        console.log("After load: " + destination.index)
+        this.setState({
+            current: destination.index
+        })
+    }
+    render() {
+        return (
+            <>
+                <h1 style={{
+                    position: 'absolute',
+                    zIndex: 10
+                }}>
+                    {this.state.current}
+                </h1>
+                <div className="lines">
+                    <div className="line"></div>
+                    <div className="line"></div>
+                    <div className="line"></div>
+                </div>
+                
+            <ReactFullpage
+                scrollOverflow={true}
+                //sectionsColor={["orange", "purple", "green"]}
+                onLeave={this.onLeave.bind(this)}
+                afterLoad={this.afterLoad.bind(this)}
+                render={({ state, fullpageApi }) => {
+                    return (
+                        <div id="fullpage-wrapper">
+                            <div className="section section1">
+                                <h3>Section 1</h3>
+                            </div>
+                            <div className="section">
+                                <div className="slide">
+                                    <h3>Slide 2.1</h3>
+                                </div>
+                                <div className="slide">
+                                    <h3>Slide 2.2</h3>
+                                </div>
+                                <div className="slide">
+                                    <h3>Slide 2.3</h3>
+                                </div>
+                            </div>
+                            <div className="section">
+                                <h3>Section 3</h3>
+                                <button onClick={() => fullpageApi.moveTo(1, 0)}>
+                                    Move top
+                                </button>
+                            </div>
+                        </div>
+                    );
+                }}
+            />
+            </>
+        );
+    }
 }
 
-export default App;
+
+
+export default App
